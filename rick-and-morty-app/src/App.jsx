3eed337a1bd61ec0b40031d58978436852fb2603
KgitWH21 <kwhayden11@gmail.com>
2026-07-20 08:29:53 -0700

    set up app skeleton

---
+++ rick-and-morty-app/src/App.jsx
@@ -0,0 +1,17 @@
+function App() {
+  return (
+    <div>
+      <header>
+        <a></a>
+        <nav>
+        </nav>
+        <main>
+        </main>
+      </header>
+    </div>
+  )
+}
+
+export default App
+
+
