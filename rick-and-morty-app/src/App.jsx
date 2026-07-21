e16b5ff8d8ac05a0f96661bafe8bab6ea1cd2b65
KgitWH21 <kwhayden11@gmail.com>
2026-07-21 11:37:30 -0700

    install axios and App.jsx connected

--- rick-and-morty-app/src/App.jsx
+++ rick-and-morty-app/src/App.jsx
@@ -1,11 +1,18 @@
+import { Outlet, NavLink, Link } from 'react-router-dom'
+
 function App() {
   return (
     <div>
       <header>
-        <a></a>
+        <a>RICK AND MORTY</a>
         <nav>
+          <NavLink as={Link} to="/">Home</NavLink>
+          <NavLink as={Link} to="/about">About</NavLink>
+          <NavLink as={Link} to="/characters">Characters</NavLink>
         </nav>
         <main>
+          {/* outlet is replaced by matching child route from router.jsx*/}
+          <Outlet />
         </main>
       </header>
     </div>
