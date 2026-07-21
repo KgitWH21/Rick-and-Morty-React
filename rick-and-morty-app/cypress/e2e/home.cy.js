9029eb5ff350c90af79cd5d7ef73237c7ac43f07
KgitWH21 <kwhayden11@gmail.com>
2026-07-21 13:57:26 -0700

    home page cypress tests written

--- rick-and-morty-app/cypress/e2e/home.cy.js
+++ rick-and-morty-app/cypress/e2e/home.cy.js
@@ -2,4 +2,16 @@ describe('HomePage', () => {
     it('renders at the root URL', () => {
         cy.visit('/')
     })
+
+    it('displays an attention getter', () => {
+        cy.visit('/')
+        cy.contains('Jump into the portal').should('be.visible')
+        cy.contains('WUBBA').should('be.visible')
+    })
+
+    it('links to About page', () => {
+        cy.visit('/')
+        cy.contains('a', 'Why watch?').click()
+        cy.url().should('include', '/about')
+    })
 })
