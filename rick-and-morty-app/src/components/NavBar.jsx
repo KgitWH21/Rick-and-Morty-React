d6a0c5e6ac4449e8e16ac48dccd7469aa42811cf
KgitWH21 <kwhayden11@gmail.com>
2026-07-22 11:44:38 -0700

    bootstrap styling for NavBar

--- rick-and-morty-app/src/components/NavBar.jsx
+++ rick-and-morty-app/src/components/NavBar.jsx
@@ -1,13 +1,23 @@
 import { NavLink } from 'react-router-dom'
+import Navbar from 'react-bootstrap/Navbar'
+import Nav from 'react-bootstrap/Nav'
 
 function NavBar() {
-    return (
-        <nav>
-          <NavLink to="/">Home</NavLink>
-          <NavLink to="/about">About</NavLink>
-          <NavLink to="/characters">Characters</NavLink>
-        </nav>
-    )
+  return (
+    <Navbar bg="dark" variant="dark" expand="lg" className="px-3">
+      <Navbar.Brand as={NavLink} to="/" className="portal-brand">
+        Rick and Morty
+      </Navbar.Brand>
+      <Navbar.Toggle aria-controls="main-nav" />
+      <Navbar.Collapse id="main-nav">
+        <Nav className="ms-auto">
+          <Nav.Link as={NavLink} to="/" end>Home</Nav.Link>
+          <Nav.Link as={NavLink} to="/about">About</Nav.Link>
+          <Nav.Link as={NavLink} to="/characters">Characters</Nav.Link>
+        </Nav>
+      </Navbar.Collapse>
+    </Navbar>
+  )
 }
 
 export default NavBar
